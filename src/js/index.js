ea8ec83059053e66ff2de65f2d398c0d15936363
carlesflor94 <cf.ferrandis@gmail.com>
2025-11-20 12:57:11 +0100

    added footer

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,9 @@
+/* to be fixed
 import Swiper from 'swiper'
 import 'swiper/css'
 import 'swiper/css/pagination'
 
-import './services.js'
+import './services.js' */
 
 import '../scss/style.scss'
 
